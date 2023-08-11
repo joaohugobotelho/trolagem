28b98e21e55a9279c0ee5accfa5504b612d76625
unknown <joaohugo202301@gmail.com>
2023-08-11 09:50:15 -0300

    pedido att

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ let widht = window.innerWidth - 150;
 button.addEventListener ('mouseover' , function () {
     button.style.position = "absolute" ;
     button.style.top = Math.random() * height + "px";
-    button.style.left = Math.ramdom() * widht + "px";
+    button.style.left = Math.random() * widht + "px";
+    button.style.right = Math.random() * height + "px";
 })
